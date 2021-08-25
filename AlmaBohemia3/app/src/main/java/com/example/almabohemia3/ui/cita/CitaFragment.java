3189879e9a1a65782347c76e46d2538bc86064d6
zolrivera <zolrivera@users.noreply.github.com>
2021-08-25 00:16:04 -0500

    cita

---
+++ AlmaBohemia3/app/src/main/java/com/example/almabohemia3/ui/cita/CitaFragment.java
@@ -0,0 +1,38 @@
+package com.example.almabohemia3.ui.cita;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.almabohemia3.R;
+
+public class CitaFragment extends Fragment {
+
+    private CitaViewModel mViewModel;
+
+    public static CitaFragment newInstance() {
+        return new CitaFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cita, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CitaViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ AlmaBohemia3/app/src/main/java/com/example/almabohemia3/ui/cita/CitaViewModel.java
@@ -0,0 +1,7 @@
+package com.example.almabohemia3.ui.cita;
+
+import androidx.lifecycle.ViewModel;
+
+public class CitaViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
